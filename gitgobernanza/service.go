9db4876c7c225b7c1bda7b98a092cdd47e226259
Codex3 <codex3@local.invalid>
2026-04-17 21:01:06 +0200

    Split git governance DB adapter

--- gitgobernanza/service.go
+++ gitgobernanza/service.go
@@ -124,55 +124,6 @@ func derefProyectoID(v *int64) int64 {
 	return *v
 }
 
-type Repository struct{}
-
-func (Repository) ListWorktrees(estado, agente string) ([]*db.Worktree, error) {
-	return db.ListarWorktrees(estado, agente)
-}
-
-func (Repository) ListLocks(estado, agente string) ([]*db.Lock, error) {
-	return db.ListarLocks(estado, agente)
-}
-
-func (Repository) GetWorktree(id int64) (*coordinacion.Worktree, error) {
-	return db.CoordinationWorktreeRepository().GetByID(id)
-}
-
-func (Repository) GetLock(id int64) (*coordinacion.Lock, error) {
-	return db.CoordinationLockRepository().GetByID(id)
-}
-
-func (Repository) ResolveProyectoIDBySlug(slug string) (*int64, error) {
-	return db.ResolveProyectoIDBySlug(slug)
-}
-
-func (Repository) GetGitMerge(id int64) (*GitMerge, error) {
-	item, err := db.GetGitMerge(id)
-	if err != nil || item == nil {
-		return nil, err
-	}
-	return mapGitMerge(item), nil
-}
-
-func (Repository) SaveGitMerge(m *GitMerge) (int64, error) {
-	return db.GuardarGitMerge(mapDBGitMerge(m))
-}
-
-func (Repository) ListGitMerges(proyectoID *int64, estado string) ([]*GitMerge, error) {
-	items, err := db.ListarGitMerges(proyectoID, estado)
-	if err != nil {
-		return nil, err
-	}
-	out := make([]*GitMerge, 0, len(items))
-	for _, item := range items {
-		if item == nil {
-			continue
-		}
-		out = append(out, mapGitMerge(item))
-	}
-	return out, nil
-}
-
 func mapGitMerge(in *db.GitMerge) *GitMerge {
 	if in == nil {
 		return nil
